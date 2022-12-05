7287e820c66ffa2dbed7b3e27029eacc3e66ea07
dongyu.cai <458498007@qq.com>
2022-12-05 11:17:06 +0800

    Initial Commit

---
+++ Shoppping-cart/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Shoppping-cart
+//
+//  Created by cai dongyu on 2022/12/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shoppping-cart/Shoppping_cartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoppping_cartApp.swift
+//  Shoppping-cart
+//
+//  Created by cai dongyu on 2022/12/5.
+//
+
+import SwiftUI
+
+@main
+struct Shoppping_cartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
